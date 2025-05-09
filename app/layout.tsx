945ac69501c2f1ccd8f24a503cb7a2dfc6b3da13
Charles Zhang <shhwkk54@gmail.com>
2025-05-09 00:07:48 -0700

    feat: add google tag script

--- app/layout.tsx
+++ app/layout.tsx
@@ -156,6 +156,20 @@ export default function RootLayout({ children }: RootLayoutProps) {
         )}
         <Toaster position="top-center" />
         <GoogleAnalytics gaId="G-G3MCW7WFM7" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16988843566"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16988843566');
+            `,
+          }}
+        />
       </body>
     </html>
   )
