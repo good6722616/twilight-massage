a0f483f29caa1db7505bbd06ff730a82f512e03e
Charles Zhang <shhwkk54@gmail.com>
2025-07-30 20:39:22 -0700

    feat: add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,10 +94,31 @@ export default function RootLayout({ children }: RootLayoutProps) {
           href="/twilight_logo_black_wotext.png"
           type="image/png"
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKZDFSTF');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${urbanist.className} flex min-h-screen flex-col bg-background text-primary`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKZDFSTF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
