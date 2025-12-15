44a426e038260b7c72477ff38d5c160f7a0ea027
Charles Zhang <shhwkk54@gmail.com>
2025-12-15 00:41:27 -0800

    chore: update nextjs version and sitemap for seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,11 +8,15 @@ module.exports = {
       {
         userAgent: "*",
         allow: "/",
+        disallow: ["/admin/*"], // 禁止搜索引擎抓取 admin 路径
       },
     ],
     additionalSitemaps: ["https://twilightmassage.com/server-sitemap.xml"],
   },
-  exclude: ["/server-sitemap.xml"],
+  exclude: [
+    "/server-sitemap.xml",
+    "/admin/*", // 排除所有 admin 路径
+  ],
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 7000,
